4819e81ffd8c4d6be57c8039b28496b5a2499cc1
tan.truong <tan.truong@paradox.ai>
2023-11-10 15:46:29 +0700

    update: Lighten the dockerfile & update: migrate the UserDocument to common

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CurrentUser } from '../decorators/current-user.decorator';
-import { UserDocument } from './models/users.schema';
+import { UserDocument } from '@app/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('users')
@@ -10,6 +10,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
     return this.usersService.create(createUserDto);
   }
 
